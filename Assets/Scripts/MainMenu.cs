949d9f79fed143c56b917aa15e9173e64d52b73a
Dominick Wiley <dman9532@gmail.com>
2022-10-10 23:31:00 -0600

    What little code I have to start with

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string levelOne;
+
+    public string howToPlay;
+
+    public string options;
+
+    public void Play()
+    {
+        SceneManager.LoadScene(levelOne);
+    }
+
+    public void HowToPlay()
+    {
+        SceneManager.LoadScene(howToPlay);
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene(options);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
