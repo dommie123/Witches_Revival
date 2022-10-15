ca7ce3294695308362416c2fd49afd09771513f3
Dominick Wiley <dman9532@gmail.com>
2022-10-14 20:24:31 -0600

    Added ghost and witch art, simple RTS selection system implemented

--- Assets/Scripts/RTSController.cs
+++ Assets/Scripts/RTSController.cs
@@ -5,13 +5,15 @@ using CodeMonkey.Utils;
 
 public class RTSController : MonoBehaviour
 {
-    [SerializeField] private GameUtils utils;
-    private Vector3 startPosition;
+    [SerializeField] private Transform selectionAreaTransform;
 
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 startPosition;
+    private List<Survivor> selectedSurvivors;
+
+    private void Awake() 
     {
-        
+        selectedSurvivors = new List<Survivor>();
+        selectionAreaTransform.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -24,18 +26,52 @@ public class RTSController : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))    // If left mouse button is pressed
         {
-            startPosition = utils.GetMouseWorldPosition3D();
+            startPosition = UtilsClass.GetMouseWorldPosition();
+            selectionAreaTransform.gameObject.SetActive(true);
+        }
+
+        if (Input.GetMouseButton(0))        // If left mouse button is held down
+        {
+            Vector3 currentMousePosition = UtilsClass.GetMouseWorldPosition();
+            Vector3 lowerLeft = new Vector3(
+                Mathf.Min(startPosition.x, currentMousePosition.x),
+                Mathf.Min(startPosition.y, currentMousePosition.y)
+            );
+            Vector3 upperRight = new Vector3(
+                Mathf.Max(startPosition.x, currentMousePosition.x),
+                Mathf.Max(startPosition.y, currentMousePosition.y)
+            );
+
+            selectionAreaTransform.position = lowerLeft;
+            selectionAreaTransform.localScale = upperRight - lowerLeft;
         }
 
         if (Input.GetMouseButtonUp(0))      // If left mouse button is released
         {   
-            Debug.Log(utils.GetMouseWorldPosition3D() + " " + startPosition);
-            Collider[] colliderArray = Physics.OverlapBox(startPosition, utils.GetMouseWorldPosition3D());
-            Debug.Log("==========");
-            foreach (Collider collider in colliderArray)
+            selectionAreaTransform.gameObject.SetActive(false);
+
+            Collider2D[] colliderArray = Physics2D.OverlapAreaAll(startPosition, UtilsClass.GetMouseWorldPosition());
+
+            // Deselect all units before clearing the list.
+            foreach (Survivor survivor in selectedSurvivors)
             {
-                Debug.Log(collider);
+                survivor.SetSelectedVisible(false);
             }
+
+            selectedSurvivors.Clear();
+
+            // Select all units within selection area
+            foreach (Collider2D collider in colliderArray)
+            {
+                Survivor survivor = collider.GetComponent<Survivor>();
+                if (survivor != null)
+                {
+                    survivor.SetSelectedVisible(true);
+                    selectedSurvivors.Add(survivor);
+                }
+            }
+
+            Debug.Log($"Survivors selected: {selectedSurvivors.Count}");
         }
     }
 }
