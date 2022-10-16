cea4960447e383f4487a63f551ff6974f3554d1b
Dominick Wiley <dman9532@gmail.com>
2022-10-16 15:12:05 -0600

    Witch now patrols area and chases players

---
+++ Assets/Scripts/LineOfSight.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineOfSight : MonoBehaviour
+{
+    public enum Direction {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    private Direction direction;
+    private Collider2D parentCollider;
+    private List<GameObject> objectsSighted;
+    
+    [SerializeField] private string targetTag;
+    [SerializeField] private Transform parent;
+    [SerializeField] private float sightRange;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        parentCollider = parent.GetComponent<Collider2D>();
+        objectsSighted = new List<GameObject>();
+        direction = Direction.Right;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dirVector = GetDirectionAsVector3();
+        RaycastHit2D[] results = new RaycastHit2D[10];
+
+        int hitCount = parentCollider.Cast(dirVector, results, sightRange);
+        Debug.DrawRay(parentCollider.transform.position, dirVector * sightRange, Color.blue, 0f, false);
+        // Debug.Log(hitCount);
+
+        objectsSighted.Clear();
+        if (hitCount == 0)
+        {
+            return;
+        }
+
+        foreach (RaycastHit2D hit in results)
+        {
+            // Debug.Log(hit.rigidbody);
+            if (!hit.collider)
+            {
+                continue;
+            }
+
+            if (hit.collider.gameObject.tag == targetTag)
+            {
+                objectsSighted.Add(hit.collider.gameObject);
+            }
+        }
+
+    }
+
+    public List<GameObject> GetObjectsInSight()
+    {
+        return objectsSighted;
+    }
+
+    public void SetDirection(Direction dir)
+    {
+        direction = dir;
+    }
+
+    public Vector3 GetDirectionAsVector3()
+    {
+        switch (direction)
+        {
+            case Direction.Up: 
+                return Vector3.up;
+            case Direction.Down:
+                return Vector3.down;
+            case Direction.Left:
+                return Vector3.left;
+            case Direction.Right:
+                return Vector3.right;
+            default:
+                return Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Scripts/Witch.cs
@@ -0,0 +1,161 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+
+public class Witch : MonoBehaviour
+{
+    protected enum PatrolState 
+    {
+        Patrolling, 
+        Idle, 
+        Chasing
+    }
+
+    private PatrolState patrolState;
+    private Vector3 currentWaypoint;
+    private LineOfSight lineOfSight;
+    private Vector3 lastKnownPosition;
+    private IMovePosition movePosition;
+    private Rigidbody2D body;
+    private bool isTouchingWall;
+
+    [SerializeField] private float patrolRange;
+
+    private void Awake() 
+    {
+        lineOfSight = transform.Find("LineOfSight").GetComponent<LineOfSight>();
+        movePosition = GetComponent<IMovePosition>();
+        body = GetComponent<Rigidbody2D>();
+        patrolState = PatrolState.Patrolling;
+        currentWaypoint = transform.position;
+        lastKnownPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        List<GameObject> objects = lineOfSight.GetObjectsInSight();
+
+        LookForPlayers(objects);
+        UpdateDirection();
+    }
+
+    public void SetLastKnownPosition(Vector3 position)
+    {
+        lastKnownPosition = position;
+        patrolState = PatrolState.Chasing;
+    }
+
+    private void LookForPlayers(List<GameObject> objects)
+    {
+        if (objects.Count == 0)
+        {
+            // Debug.Log("No objects in sight!");
+            if (patrolState == PatrolState.Chasing)
+            {
+                // Move to last known position until either survivors are found or no survivors can be seen.
+                movePosition.SetMovePosition(lastKnownPosition);
+
+                if (IsNearPosition(lastKnownPosition))
+                {
+                    // lastKnownPosition = null;
+                    patrolState = PatrolState.Patrolling;
+                }
+            }
+            else if (patrolState == PatrolState.Patrolling)
+            {
+                PatrolArea();
+            }
+
+            return;
+        }
+
+        patrolState = PatrolState.Chasing;
+        foreach (GameObject obj in objects)
+        {
+            // Debug.Log(obj);
+            if (lastKnownPosition == null)
+            {
+                lastKnownPosition = obj.transform.position;
+            }
+        }
+    }
+
+    private void UpdateDirection()
+    {
+        Vector3 moveDirection = body.velocity.normalized;
+
+        float horizontalMovement = moveDirection.x;
+        float verticalMovement = moveDirection.y;
+
+        if (math.abs(horizontalMovement) > math.abs(verticalMovement))
+        {
+            lineOfSight.SetDirection(
+                (horizontalMovement < 0) 
+                ? LineOfSight.Direction.Left
+                : LineOfSight.Direction.Right
+            );
+        }
+        else
+        {
+            lineOfSight.SetDirection(
+                (verticalMovement < 0) 
+                ? LineOfSight.Direction.Down
+                : LineOfSight.Direction.Up
+            );
+        }
+    }
+
+    private void PatrolArea()
+    {
+        Vector3 patrolPoint = 
+            (lastKnownPosition == null)
+            ? transform.position
+            : lastKnownPosition;
+
+        
+        if (IsNearPosition(currentWaypoint) || isTouchingWall)
+        {
+            isTouchingWall = false;
+            SetRandomDirection();
+            currentWaypoint = lineOfSight.GetDirectionAsVector3() * patrolRange;
+        }
+
+        movePosition.SetMovePosition(currentWaypoint);
+    }
+
+    private void SetRandomDirection()
+    {
+        int selectedNumber = (int) math.round(UnityEngine.Random.Range(0, 4));
+
+        switch (selectedNumber)
+        {
+            case 0:
+                lineOfSight.SetDirection(LineOfSight.Direction.Left);
+                break;
+            case 1: 
+                lineOfSight.SetDirection(LineOfSight.Direction.Right);
+                break;
+            case 2: 
+                lineOfSight.SetDirection(LineOfSight.Direction.Up);
+                break;
+            default:
+                lineOfSight.SetDirection(LineOfSight.Direction.Down);
+                break;
+        }
+    }
+
+    private bool IsNearPosition(Vector3 position)
+    {
+        Debug.Log(Vector3.Distance(transform.position, position));
+        return Vector3.Distance(transform.position, position) < 2.5f;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        if (other.gameObject.tag == "Wall")
+            isTouchingWall = true;
+    }
+}
+
