3fc4ad3c7928035373743ee7f62d96c9aa5f5f0f
Dominick Wiley <dman9532@gmail.com>
2022-10-18 18:02:01 -0600

    Got portals working, added map, portal, and hiding spot assets

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -4,18 +4,52 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
+    private Portal[] portals;
+
     [SerializeField] private int sourceIndex;
     [SerializeField] private int targetIndex;
+    [SerializeField] private LayerMask entityMask;
     
     [SerializeField] public bool PlayerIsExiting {get; set;}
 
     private void Awake() 
     {
-        
+        portals = Object.FindObjectsOfType<Portal>();
     }
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
+        if (other.gameObject.layer != 3)
+            return;
         
+        if (PlayerIsExiting)
+            return;
+        
+        Portal exitPortal = null;
+
+        foreach (Portal portal in portals)
+        {
+            if (portal.GetSourceIndex() == targetIndex)
+            {
+                exitPortal = portal;
+                break;
+            }
+        }
+
+        if (exitPortal == null)
+            return;
+        
+        exitPortal.PlayerIsExiting = true;
+        other.transform.position = exitPortal.transform.position;   // Teleport entity to portal's position
+    }
+
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+        PlayerIsExiting = false;    // This helps to make the portals two-way
+    }
+
+    public int GetSourceIndex()
+    {
+        return sourceIndex;
     }
 }
